5ea2a5a53d3a1e734afbf5d529b265d84a94f6b4
Ridicc <franek.razny@gmail.com>
2024-09-28 17:53:55 +0200

    activity init

--- lib/appState/mainAppState.dart
+++ lib/appState/mainAppState.dart
@@ -4,6 +4,16 @@ import 'package:provider/provider.dart';
 class MainAppState extends ChangeNotifier {
   int navBarIndex = 0;
 
+  var activities = [
+    "cycling",
+    "skiing",
+    "workout",
+    "pushups",
+    "running",
+    "walking",
+    "standing"
+    ];
+
   void navBarTap(int index) {
     navBarIndex = index;
     notifyListeners();
